c8822bf47c4c57674358a53dee68eee2a277fa72
Nadezhda <furman_nadya@mail.ru>
2022-04-22 09:52:49 +0600

    sql

--- Запросы.sql
+++ Запросы.sql
@@ -1,3 +1,4 @@
+/*Создание базы данных "Одежда"*/
 CREATE TABLE clothes (
  
 id INT NOT NULL AUTO_INCREMENT, 
@@ -8,10 +9,12 @@ PRIMARY KEY (id));
 
 
 
+/*Вывод всей БД "Одежда"*/
 SELECT * FROM clothes;
 
 
 
+/*Заполнение БД */
 INSERT INTO clothes (clothescol) VALUES ('Рубашка');
 
 INSERT INTO clothes (clothescol) VALUES ('Футболка');
@@ -30,16 +33,20 @@ INSERT INTO clothes (clothescol) VALUES ('Пиджак');
 
 
 
+/*Вывести часть БД, где id > 3. Ограничить тремя строками*/ 
 SELECT * FROM clothes WHERE id > 3 LIMIT 3;
 
 
 
+/*Обновление БД, где id = 6*/
 UPDATE clothes SET clothescol = 'Копроновые колготки' WHERE id = 6;
 
 
 
+/*Удаление части БД, где id > 7*/
 DELETE FROM clothes WHERE id > 7;
 
 
 
+/*Вывести БД в отсортированном по убыванию по id виде*/
 SELECT * FROM clothes ORDER BY id DESC;
